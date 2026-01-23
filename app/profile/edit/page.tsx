01523dca76da54c8a460745dfbc8bcfb5b771868
gopinathmurugan-39 <gopinathmurugan.work@gmail.com>
2026-01-23 11:05:37 +0530

    Photo upload for edit profile completed
    Added image configs too

--- app/profile/edit/page.tsx
+++ app/profile/edit/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import PhotoUpload from "@/components/PhotoUpload";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function EditProilePage() {
+	const [formData, setFormData] = useState({
+		full_name: "",
+		username: "",
+		bio: "",
+		gender: "male" as "male" | "female" | "other",
+		birthdate: "",
+		avatar_url: ""
+	})
 	return (
 		<div className="min-h-screen bg-gradient-to-br from-pink-50 to-red-50 dark:from-gray-900 dark:to-gray-800">
 			<div className="container mx-auto px-4 py-8">
@@ -21,9 +32,22 @@ export default function EditProilePage() {
 							<div className="flex items-center space-x-6">
 								<div className="relative">
 									<div className="w-24 h-24 rounded-full overflow-hidden">
-										<Image className="w-full h-full object-cover" width={100} height={100} src={"/default-avatar.png"} alt={""}/>
+										<Image className="w-full h-full object-cover" width={100} height={100} src={formData.avatar_url === "" ? "/default-avatar.png" : formData.avatar_url} alt={""}/>
 									</div>
-									<PhotoUpload onPhotoUploaded={() => null} />
+									<PhotoUpload onPhotoUploaded={(url) => {
+										setFormData((prev) => ({ 
+											...prev,
+											avatar_url: url
+										 }))
+									}} />
+								</div>
+								<div>
+									<p className="text-sm text-gray-600 dark:text-gray-400 mb-2">
+										Upload a new profile picture
+									</p>
+									<p className="text-xs text-gray-500 dark:text-gray-500">
+										JPG, PNG or GIF. Max 5MB.
+									</p>
 								</div>
 							</div>
 						</div>
--- lib/actions/profile.ts
+++ lib/actions/profile.ts
@@ -20,3 +20,38 @@ export async function GetCurrentUserProfile() {
 
 	return profile;
 }
+
+export async function uploadProfilePhoto(file: File) {
+	const supabase = await createClient();
+
+	const {data: {user}} = await supabase.auth.getUser();
+
+	if(!user) {
+		return {
+			success: false,
+			error: "User is not logged in"
+		}
+	}
+
+	const fileExt = file.name.split(".").pop();
+	const fileName = `${user.id}-${Date.now()}.${fileExt}`;
+
+	const {error} = await supabase.storage.from("profilepics").upload(fileName, file, {
+		cacheControl: "3600",
+		upsert: false
+	});
+
+	if(error) {
+		return {
+			success: false,
+			error: error.message
+		}
+	}
+
+	const {data: {publicUrl}} = supabase.storage.from("profilepics").getPublicUrl(fileName);
+
+	return {
+		success: true,
+		url: publicUrl
+	}
+}
